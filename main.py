267461947e6ef476f9507242649d43eb29d62d3b
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-24 11:20:52 +0000

    imported the dotenv package and called the login_parameters method

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+from dotenv import load_dotenv
+import os
 from linkedin import LinkedInScrapper
+
+load_dotenv()
+
+my_username = os.environ.get("MY_USERNAME")
+my_password = os.environ.get("MY_PASSWORD")
 
 with LinkedInScrapper() as bot:
     bot.landing_page()
-    bot.jobs_page()
-    bot.select_jobs_or_companies('web developer')
-    bot.search_locations('Ghana')
-    bot.search_submit()
-    bot.select_experience_level('Internship','Entry')
+    bot.login_parameters(my_username,my_password)
     bot.refresh()
+    # bot.jobs_page()
+    # bot.select_jobs_or_companies('web developer')
+    # bot.search_locations('Ghana')
+    # bot.search_submit()
+    # bot.select_experience_level('Internship','Entry')
     # takes a number of the results you want
-    bot.job_results()
+    # bot.job_results()
+
