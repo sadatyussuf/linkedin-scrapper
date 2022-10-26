5e9e4c2fb932a0eec8c30f7bb9c3f68dc7fd7afc
sadatyussuf <tommyleespartan0@gmail.com>
2022-10-26 09:37:08 +0000

    working on select_experience_level method

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ my_password = os.environ.get("MY_PASSWORD")
 with LinkedInScrapper() as bot:
     bot.landing_page()
     bot.login_parameters(my_username,my_password)
-    bot.refresh()
+    # bot.refresh()
+    bot.set_jobs_page()
     # bot.jobs_page()
-    # bot.select_jobs_or_companies('web developer')
-    # bot.search_locations('Ghana')
-    # bot.search_submit()
-    # bot.select_experience_level('Internship','Entry')
+    bot.select_jobs_or_companies('web developer')
+    bot.search_locations('Ghana')
+    bot.search_submit()
+
+    bot.select_experience_level('Internship','Entry')
     # takes a number of the results you want
     # bot.job_results()
 
